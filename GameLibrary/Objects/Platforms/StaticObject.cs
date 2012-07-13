a0cdc0b24b1814a079e46cad67047d936ef43b36
BenGriffiths <scare_tfc@hotmail.com>
2012-07-13 03:34:01 +0100

    Uses rotation instead of translated w/h

--- GameLibrary/Objects/Platforms/StaticObject.cs
+++ GameLibrary/Objects/Platforms/StaticObject.cs
@@ -454,9 +454,10 @@ namespace GameLibrary.Objects
             //  This function will have to be changed if we have things that aren't going to be square/rectangles.
             //  Fortunately, Farseer will allow us to use the Texture to find the outline. Haven't tried it with 
             //  full coloured images, only outlines. Will have to research! More demos!
-            this.Body = BodyFactory.CreateRectangle(world, ConvertUnits.ToSimUnits(this.Width), ConvertUnits.ToSimUnits(this.Height), ConvertUnits.ToSimUnits(this.Mass), ConvertUnits.ToSimUnits(this.Position));
+            this.Body = BodyFactory.CreateRectangle(world, ConvertUnits.ToSimUnits(_width), ConvertUnits.ToSimUnits(_height), ConvertUnits.ToSimUnits(this.Mass), ConvertUnits.ToSimUnits(this.Position));
             //  Give it a 0 for no flags.
             this.Body.FixtureList[0].UserData = (int)0;
+            this.Body.Rotation = _rotation;
             // Elastic (>1) <->Non-elastic collisions (0).
             this.Body.Restitution = 0f;
             this.Body.CollisionCategories = Category.Cat20;
