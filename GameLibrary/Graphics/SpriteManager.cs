6e74331ff0faa188ca1ce5fa8f73f0d4530770ac
BenGriffiths <scare_tfc@hotmail.com>
2012-07-25 15:35:09 +0100

    Proper commenting

--- GameLibrary/Graphics/SpriteManager.cs
+++ GameLibrary/Graphics/SpriteManager.cs
@@ -27,67 +27,195 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using GameLibrary.Graphics.Drawing;
+using GameLibrary.GameLogic;
 
 namespace GameLibrary.Graphics
 {
     public class SpriteManager
     {
-        private static SpriteManager _singleton = null;
+        #region Fields and Properties
+
+        #region Singleton and Getter
+
+
+        /// <summary>
+        /// The singleton
+        /// </summary>
+        private static SpriteManager _singleton = new SpriteManager();
+
+        /// <summary>
+        /// Gets the singleton instance of the SpriteManager.
+        /// </summary>
         public static SpriteManager Instance
         {
             get
             {
-                if (_singleton == null)
-                {
-                    _singleton = new SpriteManager();
-                }
-
                 return _singleton;
             }
         }
 
-        #region Variables
-
-        private List<Sprite> SpriteList = new List<Sprite>();
 
         #endregion
 
-        public void Update(GameTime gameTime)
-        {
-            for (int i = SpriteList.Count - 1; i > 0; i--)
-            {
-                SpriteList[i].Update(gameTime);
+        #region Master SpriteList
 
-                if (SpriteList[i].IsDead == true)
+
+        /// <summary>
+        /// The main list to hold all the sprites
+        /// </summary>
+        private List<Sprite> _spriteList;
+
+
+        #endregion
+
+        #region SpriteList to Draw
+
+        /// <summary>
+        /// We want to draw all the sprites in the list when the draw is called.
+        /// If a sprite is removed after, it can throw.
+        /// </summary>
+        private List<Sprite> _spritesToDraw;
+
+        #endregion
+
+        #region ScreenManager
+
+        private ScreenManager _screenManager;
+
+        #endregion
+
+        #endregion
+
+        #region Constructor
+
+
+        /// <summary>
+        /// Instantiated through the singleton, so we can't pass anything in.
+        /// </summary>
+        private SpriteManager()
+        {
+            this._spriteList = new List<Sprite>();
+            this._spritesToDraw = new List<Sprite>();
+        }
+
+
+        #endregion
+
+        #region Load / Setup the SpriteManager
+
+
+        public void Load(ScreenManager screenManager)
+        {
+            //  First we'll make sure that the new SpriteList is empty
+            //  incase load has been called from a level change.
+            this.Clear();
+
+            //  We'll pass in the ScreenManager mainly for the Game and 
+            //  graphicsdevice references.
+            this._screenManager = screenManager;
+        }
+
+        #endregion
+
+        #region Update
+        /// <summary>
+        /// Update each sprite in the SpriteList
+        /// </summary>
+        /// <param name="delta">Elapsed time in seconds</param>
+        public void Update(float delta)
+        {
+            //  As sprites will be getting removed from the list, 
+            //  we'll decrement our way through the spritelist.
+            for (int i = this._spriteList.Count - 1; i > 0; i--)
+            {
+                //  Update the sprite
+                this._spriteList[i].Update(delta);
+
+                //  Then check if it's dead and needs removing.
+                if (this._spriteList[i].IsDead == true)
                 {
-                    SpriteList.RemoveAt(i);
+                    //  If it is dead, remove it from the _spritelist
+                    this._spriteList.RemoveAt(i);
                 }
             }
         }
-
-        #region AddSprites
-        public void AddSprite(Sprite sprite)
-        {
-            SpriteList.Add(sprite);
-        }
         #endregion
 
-        public int ListCount()
-        {
-            return SpriteList.Count();
-        }
+        #region Draw
 
-        public void Draw(SpriteBatch sb)
+
+        /// <summary>
+        /// Draw whats currently in the SpriteList to screen.
+        /// </summary>
+        /// <param name="SpriteBatch">Current SpriteBatch to render to</param>
+        public void Draw(SpriteBatch spriteBatch)
         {
-            for (int i = SpriteList.Count - 1; i > 0; i--)
+#if !EDITOR
+            //  Make sure the draw sprite list is empty.
+            this._spritesToDraw.Clear();
+
+            //  then copy over what's currently in the master sprite list.
+            this._spritesToDraw.AddRange(_spriteList);
+
+            //  Go through the list and draw them all
+            for (int i = 0; i < _spritesToDraw.Count; i++)
             {
-                SpriteList[i].Draw(sb);
+                _spritesToDraw[i].Draw(spriteBatch, this._screenManager.GraphicsDevice);
             }
+#endif
         }
 
+
+        #endregion
+
+        #region Extra Methods
+
+
+        #region Clear the SpriteList
+
+
+        /// <summary>
+        /// Empties the SpriteList.
+        /// To be called from Load and maybe elsewhere.
+        /// </summary>
         public void Clear()
         {
-            SpriteList = new List<Sprite>();
+            _spriteList = new List<Sprite>();
         }
+
+        #endregion
+
+
+        #region SpriteList Count
+        /// <summary>
+        /// Returns the amount of sprites that are in the list.
+        /// </summary>
+        /// <returns>How many sprites are in the SpriteList</returns>
+        public int ListCount()
+        {
+            return _spriteList.Count();
+        }
+
+        #endregion
+
+
+        #region AddSprites
+
+
+        /// <summary>
+        /// Add a sprite to the SpriteManagers master list
+        /// </summary>
+        /// <param name="sprite">The sprite to be added. Must be init'd and loaded.</param>
+        public void AddSprite(Sprite sprite)
+        {
+            //  Add the sprite to the list.
+            _spriteList.Add(sprite);
+        }
+
+
+        #endregion
+
+
+        #endregion
     }
 }
