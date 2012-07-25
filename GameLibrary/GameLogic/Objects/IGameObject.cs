772767e0d618857863a7bed6850b4c67379d513b
BenGriffiths <scare_tfc@hotmail.com>
2012-07-25 15:29:21 +0100

    Allows object hook up to events

---
+++ GameLibrary/GameLogic/Objects/IGameObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+
+namespace GameLibrary.GameLogic.Objects
+{
+    public interface IGameObject
+    {
+        string Name { get; }
+    }
+}
--- GameLibrary/GameLogic/Objects/NodeObject.cs
+++ GameLibrary/GameLogic/Objects/NodeObject.cs
@@ -7,22 +7,41 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using System.ComponentModel;
 using FarseerPhysics.Dynamics;
+using GameLibrary.GameLogic.Screens;
+using GameLibrary.GameLogic.Events;
 
 namespace GameLibrary.GameLogic.Objects
 {
-    public class NodeObject : ICloneable
+    public class NodeObject : ICloneable, IGameObject
     {
         #region Fields
+
+        [ContentSerializer(Optional = true)]
+        protected string _name;
         [ContentSerializer]
         protected Vector2 _position;
         [ContentSerializer]
         protected float _zLayer;
+        [ContentSerializer(Optional = true)]
+        protected List<Event> _objectEvents = new List<Event>();
 
         #endregion
 
         #region Properties
 
 #if EDITOR
+        [ContentSerializerIgnore, CategoryAttribute("Events")]
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
         [ContentSerializerIgnore, CategoryAttribute("General")]
         public Vector2 Position
         {
@@ -67,14 +86,42 @@ namespace GameLibrary.GameLogic.Objects
             }
             set { }
         }
-
+        [ContentSerializerIgnore, CategoryAttribute("Events")]
+        public List<Event> EventList
+        {
+            get
+            {
+                return _objectEvents;
+            }
+            set
+            {
+                _objectEvents = value;
+            }
+        }
 
 #else
+        [ContentSerializerIgnore]
         public Vector2 Position
         {
             get
             {
                 return _position;
+            }
+        }
+        [ContentSerializerIgnore]
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        [ContentSerializerIgnore]
+        public List<Event> ObjectEvents
+        {
+            get
+            {
+                return _objectEvents;
             }
         }
 #endif
@@ -95,15 +142,23 @@ namespace GameLibrary.GameLogic.Objects
         public virtual void Load(ContentManager content, World world)
         { }
 
-        public virtual void Update(GameTime gameTime)
+        public virtual void Update(float delta)
         {
 
         }
 
+        #region Draw
+#if EDITOR
         public virtual void Draw(SpriteBatch sb)
         {
+        }
+#else
+        public virtual void Draw(SpriteBatch sb, GraphicsDevice graphics)
+        {
 
         }
+#endif
+        #endregion
 
         #region Clone
         object ICloneable.Clone()
@@ -115,6 +170,29 @@ namespace GameLibrary.GameLogic.Objects
         {
             return (NodeObject)this.MemberwiseClone();
         }
+        #endregion
+
+        #region Event Stuff
+#if !EDITOR
+
+
+        protected void RegisterEvent()
+        {
+            EventManager.Instance.RegisterObject(this);
+        }
+
+        protected void FireEvent() 
+        {
+            EventManager.Instance.FireEvent(this._name);
+        }
+#endif
+
+        public virtual void Toggle() { }
+        public virtual void Enable() { }
+        public virtual void Disable() { }
+        public virtual void Start() { }
+        public virtual void Stop() { }
+
         #endregion
     }
 }
