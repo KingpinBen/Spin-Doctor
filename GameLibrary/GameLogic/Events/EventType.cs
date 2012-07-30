faff6ca7ab2f70109d95164d27918f98b82eeb8e
BenGriffiths <scare_tfc@hotmail.com>
2012-07-30 15:34:32 +0100

    Added rotation triggers

--- GameLibrary/GameLogic/Events/EventType.cs
+++ GameLibrary/GameLogic/Events/EventType.cs
@@ -7,25 +7,28 @@ namespace GameLibrary.GameLogic.Events
 {
     public enum EventType
     {
-        // Used for targets
+        /// <summary>
+        /// Used by the event manager to declare a target with no event output.
+        /// </summary>
         NONE,   
 
-        //  Generic
-        ON_TOUCH,
-        ON_LEAVE,
-        ON_KILL,
-        ON_ACTIVATION,
-        ON_DEACTIVATION,
-
+        /// <summary>
+        /// Handled by the object.
+        /// </summary>
         TRIGGER_ENABLE,
         TRIGGER_DISABLE,
-
         TRIGGER_START,
         TRIGGER_STOP,
         TRIGGER_CHANGESPEED,
         TRIGGER_TOGGLE,
 
+        /// <summary>
+        /// Handled through EventManager rather than the object.
+        /// </summary>
         TRIGGER_REMOVE,
+        ROTATE_CW90,
+        ROTATE_CCW90,
+        ROTATE_180,
 
         //  System
         CHANGE_LEVEL,
