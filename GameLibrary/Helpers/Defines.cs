549be44b6d1434b6071aa7a0ef2e81885d1fad45
BenGriffiths <scare_tfc@hotmail.com>
2012-08-05 20:59:17 +0100

    Made global defines

---
+++ GameLibrary/Helpers/Defines.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLibrary.Helpers
+{
+    public static class Defines
+    {
+        private static string _levelDirectory = "Content/Assets/Levels/level";
+        public static string Level(int id)
+        {
+            return _levelDirectory + id + ".xml";
+        }
+
+        public static string DEVELOPMENT_TEXTURE = "Assets/Other/Dev/Trigger";
+        public const string BLANK_PIXEL = "Assets/Images/Basics/BlankPixel";
+        public const string NOTE_DIRECTORY = "Assets/Other/Notes/Note_";
+
+        public const float PLAYER_RUN_SPEED = 16.0f;
+        public const float PLAYER_JUMP_FORCE = 4.2f;
+        public const float PLAYER_MIDAIR_FORCE = 200.0f;
+
+    }
+}
